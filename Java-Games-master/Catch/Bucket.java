8036a329849fb202a8f72887aefd7cba32202910
Yodaaa <129697705+Mamayoda@users.noreply.github.com>
2024-04-17 17:04:27 -0400

    Update Bucket.java
    se agregaron los nuevos colores del cuadrado

--- Java-Games-master/Catch/Bucket.java
+++ Java-Games-master/Catch/Bucket.java
@@ -7,21 +7,28 @@ public class Bucket
 	public static final int SPEED = 5;
 
 	private int x,y;
+	private Color color;
 
-	public Bucket(int x, int y)
+	public static final Color BLACK_COLOR = Color.BLACK;
+	public static final Color BLUE_COLOR = Color.blue;
+	public static final Color ORAN_COLOR = Color.orange;
+	public static final Color PURPLE_COLOR = Color.magenta;
+
+	public Bucket(int x, int y, Color color)
 	{
 		this.x = x;
 		this.y = y;
+		this.color = color;
 	}
 
 	public Bucket moveTo(int x, int y)
 	{
-		return new Bucket(x,y);
+		return new Bucket(x,y,this.color);
 	}
 
 	public Bucket move(int dx, int dy)
 	{
-		return new Bucket(x+dx, y+dy);
+		return new Bucket(x+dx, y+dy,this.color);
 	}
 
 	public Bucket moveLeft()
@@ -51,7 +58,7 @@ public class Bucket
 
 	public void draw(Graphics g)
 	{
-		g.setColor(Color.BLACK);
+		g.setColor(color);
 		g.fillRect(x-WIDTH/2, y-HEIGHT/2, WIDTH, HEIGHT);
 	}
 }
